b11d935a5030687a04dbb9fa1339a3ae99cad2f9
alipex <80337966+alipex@users.noreply.github.com>
2023-10-24 20:47:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Spectre.Console;
+
+namespace WVMM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            
+        }
+    }
+}
